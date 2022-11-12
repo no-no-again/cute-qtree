8b0e84296c08180e2ea51b9f26e9587998248a3b
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-11-13 00:19:58 +0400

    change white to snow

--- src/colors.ts
+++ src/colors.ts
@@ -4,4 +4,4 @@ export const RED = [218, 44, 67] as RGB;
 export const BLUE = [0, 255, 0] as RGB;
 export const GREEN = [0, 0, 255] as RGB;
 export const BLACK = [0, 0, 0] as RGB;
-export const WHITE = [255, 255, 255] as RGB;
+export const WHITE = [255, 250, 250] as RGB;
