5b688a26f9813149b1a7107dc78aa91db80a2f5f
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-11-12 20:43:38 +0400

    display simulation info

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import p5, { Vector } from 'p5';
 import { Agent } from './agent';
 import { Mover } from './mover';
 import { CircleDrawer } from './drawers';
-import { BACKGROUND, HEIGHT, NAGENTS, WIDTH } from './config';
+import {
+    WIDTH,
+    HEIGHT,
+    NAGENTS,
+    AVOID_THRESHOLD,
+    BACKGROUND
+} from './config';
 
 const sketch = (s: p5) => {
     const agents: Agent[] = [];
@@ -12,7 +18,10 @@ const sketch = (s: p5) => {
         s.createCanvas(WIDTH, HEIGHT);
 
         for (let i = 0; i < NAGENTS; i++) {
-            const pos = new Vector(s.random(20, 380), s.random(20, 380));
+            const pos = new Vector(
+                s.random(AVOID_THRESHOLD, WIDTH - AVOID_THRESHOLD),
+                s.random(AVOID_THRESHOLD, HEIGHT - AVOID_THRESHOLD)
+            );
             const vel = new Vector(s.random(-0.25, 0.25), s.random(-0.25, 0.25));
             const mover = new Mover(pos, vel);
             const agent = new Agent(mover, circleDrawer);
@@ -27,7 +36,15 @@ const sketch = (s: p5) => {
             agent.update();
             agent.draw();
         }
+
+        info(s);
     }
+}
+
+const info = (s: p5) => {
+    s.textSize(10);
+    s.text(s.frameRate().toFixed(2), 10, 20);
+    s.fill(255);
 }
 
 new p5(sketch);
