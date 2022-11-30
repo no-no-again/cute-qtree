2f88535d242571baf4bc4a563a135cac9032d7fe
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2022-12-01 00:08:07 +0400

    tweak config

--- src/colors.ts
+++ src/colors.ts
@@ -1,7 +1,7 @@
 import { RGB } from "./types";
 
 export const RED = [218, 44, 67] as RGB;
-export const GREEN = [0, 255, 0] as RGB;
+export const GREEN = [51, 184, 100] as RGB;
 export const BLUE = [0, 191, 255] as RGB;
 export const BLACK = [0, 0, 0] as RGB;
 export const WHITE = [255, 250, 250] as RGB;
--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,23 @@
-import { BLUE, RED, WHITE } from "./colors";
+import { RED, GREEN, BLUE, WHITE } from "./colors";
 
 export const DEBUG = true;
 
 export const WIDTH = 650;
 export const HEIGHT = 650;
+export const TEXT_COLOR = WHITE;
 export const BACKGROUND = 0;
 
 export const QTREE_CAP = 4;
 export const QTREE_VISIBLE = false;
 
-export const N_AGENTS = 300;
+export const N_AGENTS = 50;
 export const AVOID_THRESHOLD = 15;
 
 export const AGENT_SPEED = 0.75;
-export const AGENT_RADIUS = 2;
+export const AGENT_RADIUS = 5;
 export const AGENT_COLOR = WHITE;
-export const AGENT_HIGHLIGHT_COLOR = RED;
+export const AGENT_IN_RANGE_COLOR = RED;
+export const AGENT_COLLIDED_COLOR = GREEN;
 export const AGENT_NEAR_RANGE_COLOR = BLUE;
 export const AGENT_NEAR_RANGE_VISIBLE = true;
 
